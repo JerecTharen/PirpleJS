99afec29bea74f973962d2dd60aef14b8e433ae0
JerecTharen <kf7ags@gmail.com>
2021-03-13 14:01:33 -0500

    Parsing server paths

--- index.js
+++ index.js
@@ -25,9 +25,32 @@ const _portNum = 3000;
 Server set up
 =============
 */
-//Set server to respond to all requests with a string
+//Set server to respond to requests
 const _server = _http.createServer((req, resp) =>{
-    resp.end('Hello There!\nGeneral Kenobi!!!\n');
+    let responseString = 'Hello There!\nGeneral Kenobi!!!\n';
+
+    //Get URL and parse it
+    let urlObj = _url.parse(req.url, true);//True says yes, use querystring module to get query string
+
+    //Parse the path
+    let pathnameString = urlObj.pathname;
+    //Don't care about removing head '/' characters, only care about removing trailing ones
+    let parsedPathnameString = pathnameString.slice(pathnameString.length-1, pathnameString.length) === '/' ? 
+        pathnameString.slice(0, pathnameString.length - 1).toLowerCase()
+        : pathnameString.toLowerCase();
+
+    //Send Response
+    switch(parsedPathnameString){
+        case '/hellothere':
+            resp.end('General Kenobi!!!!\n');
+            break;
+        default:
+            resp.end(responseString);
+            break;
+    }
+    
+    //Log response
+    console.log(`Received paths - full path: "${pathnameString}", parsed: "${parsedPathnameString}"`);
 });
 //Get the server to listen to our specified port
 _server.listen(_portNum, ()=>{
